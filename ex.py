db0760882371bb19de22e9f272cd2ff57bbd4aee
javier.morales <javier.morales@proservicio.com>
2023-11-17 17:18:47 -0600

    ya xfa

--- ex.py
+++ ex.py
@@ -6,7 +6,7 @@ import subprocess
 
 # Token de acceso personal de GitHub
 # Necesitas generar un token en: https://github.com/settings/tokens
-ACCESS_TOKEN = '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'
+ACCESS_TOKEN = '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'
 
 # Nombre de usuario y nombre del repositorio
 USER = 'JavierA-MorHer'
